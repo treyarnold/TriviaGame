de25c7d2c7a6a7068511a991baedb17ca4b37ba6
treyarnold <arnold.trey80@gmail.com>
2019-03-23 22:05:31 -0500

    all working except reset for new game

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,16 +10,40 @@ const trivia = [{question: "What house commited the massacre known as the Red We
                 {question: "What does Valar Morghulis mean?",
                 correct: "All men must die",
                 options: ["All men must serve", "All men must live", "All men must dance", "All men must die"]},
+                {question: "Who was the Lord Commander of the Night's watch before Jon Snow?",
+                correct: "Jeor Mormont",
+                options: ["Jorah Mormont", "Maege Mormont", "Lynesse Mormont", "Jeor Mormont"]},
+                {question: "How did The Hound's face get burned?",
+                correct: "His brother held him in a fire",
+                options: ["His brother held him in a fire", "A battle scar from the Battle of the Blackwater", "He burnt himself to show his faith", "King Joffrey did it for fun"]},
+                {question: "What is The Hound's real name?",
+                correct: "Sandor Clegane",
+                options: ["Tytos Clegane", "Gregor Clegane", "Sandor Clegane", "Tyrith Clegane"]},
+                {question: "What is the name of the butcher's boy that the Hound killed?",
+                correct: "Mycah",
+                options: ["Mrycella", "Mythos", "Melisandre", "Mycah"]},
+                {question: "Which is not a Stark?",
+                correct: "Bronn",
+                options: ["Lyanna", "Bran", "Bronn", "Rickon"]},
+                {question: "Who has not served Daenerys Targaryen?",
+                correct: "Jeor Mormont",
+                options: ["Tyrion Lanniser", "Jeor Mormont", "Doreah", "Barristan Selmy"]},
             ]
 
 const game = {
     questions: [],
     questionNumber: -1,
     timer: 10,
+    correctCount: 0, 
+    incorrectCount: 0,
 
     reset: function () {
         this.questions = trivia;
         this.questionNumber = -1;
+        this.correctCount = 0;
+        this.incorrectCount = 0;
+        $("#correct").text(this.correctCount);
+        $("#incorrect").text(this.incorrectCount);
         this.nextQuestion();
     },
 
@@ -58,33 +82,101 @@ const game = {
 
     correct: function () {
         console.log("correct")
-    },
-
-    incorrect: function () {
-        console.log("incorrect")
-    },
-
-    timeout: function () {
         let display = 
             `<hr>
-            <div class="container">
+            <div class="container text-center">
                 <div class="row">
-                    <p class="display-2 text-danger">Time's up</p>
+                    <p class="display-2 text-success">Correct!!</p>
                 </div>
-                <div class="row mt-5">
+                <div class="row mt-5 text-center">
                     <p>The correct answer was ${currentQuestion.correct}
                 </div>
             </div>`;
         $("#question").html(display);
-        setTimeout(this.nextQuestion, 5000);
+        game.correctCount++;
+        $("#correct").text(game.correctCount);
+        setTimeout(game.nextQuestion, 3000);
     },
-        
+    
+    incorrect: function () {
+        console.log("incorrect")
+        let display = 
+        `<hr>
+        <div class="container text-center">
+        <div class="row">
+        <p class="display-2 text-danger text-center">Wrong one, Dumbass</p>
+        </div>
+        <div class="row mt-5 text-center">
+        <p>The correct answer was ${currentQuestion.correct}
+        </div>
+        </div>`;
+        $("#question").html(display);
+        game.incorrectCount++;
+        $("#incorrect").text(game.incorrectCount);
+        setTimeout(game.nextQuestion, 3000);
+    },
+    
+    timeout: function () {
+        let display = 
+        `<hr>
+        <div class="container">
+        <div class="row">
+        <p class="display-2 text-danger text-center">Time's up</p>
+        </div>
+        <div class="row mt-5 text-center">
+        <p>The correct answer was ${currentQuestion.correct}
+        </div>
+        </div>`;
+        $("#question").html(display);
+        game.incorrectCount++;
+        $("#incorrect").text(game.incorrectCount);
+        setTimeout(game.nextQuestion, 3000);
+    },
+
+    gameover: function () {
+        $("#questionTitle").text(`Game Over}`)
+        let display = 
+            `<hr>
+            <div class="container">
+                <div class="row"n">
+                    <p>Here is how you did:}</p>
+                </div>
+                <div class="row justify-content-around" id="options">
+                    <div class="col-md-5 option">
+                        Correct:
+                    </div>
+                    <div class="col-md-5 option text-success">
+                        ${game.correctCount}
+                    </div>                        
+                </div>
+                <div class="row justify-content-around">
+                    <div class="col-md-5 option">
+                        Incorrect:
+                    </div>
+                    <div class="col-md-5 option text-danger">
+                        ${game.incorrectCount}
+                    </div>                        
+                </div>
+            </div>
+            <hr>
+            <div class="text-center">
+                <button id="playAgain">Play Again?</button>
+            </div>`;
+        console.log("game over");
+        $("#question").html(display);
+        $("#playAgain").on("click", game.reset());
+    },
+    
     nextQuestion: function () {
         game.questionNumber++;
+        if (game.questionNumber === 10) {
+            console.log("if gameover");
+            game.gameover();
+        }
         currentQuestion = game.questions[game.questionNumber];
         game.timer = 10;
         $("#question").html(game.questionFormat(currentQuestion));
-        let interval = setInterval(()=>{
+        let interval = setInterval(() => {
             if (game.timer > 0){
                 game.timer--;
                 $("#timer").text(game.timer);
@@ -93,7 +185,7 @@ const game = {
                 game.timeout(currentQuestion);
             }
         }, 1000);
-        $(".option").on("click", function(event) {
+        $(".option").on("click", (event) => {
             clearInterval(interval);
             if (event.target.innerText === currentQuestion.correct) {
                 game.correct();
@@ -101,6 +193,7 @@ const game = {
                 game.incorrect();
             }
         })
+        console.log(game.questionNumber);
     }
 }
 
